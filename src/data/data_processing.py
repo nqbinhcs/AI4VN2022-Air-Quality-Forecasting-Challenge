5b659672cdef8a2bdb15362e5cda562fa51a197c
Binh Q. Nguyen <quangbinh047@gmail.com>
2022-08-01 14:35:54 +0700

    Complete baseline

--- src/data/data_processing.py
+++ src/data/data_processing.py
@@ -17,15 +17,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+from tqdm import tqdm
+
 sns.set()
 warnings.filterwarnings(action='ignore')
 
-INPUT_PATH = 'data/raw/data-train/input'
+INPUT_PATH = 'data/raw/public-test/input'
+
+
+def create_date_features(df):
+    df['month'] = df.timestamp.dt.month
+    df['day_of_month'] = df.timestamp.dt.day
+    df['day_of_year'] = df.timestamp.dt.dayofyear
+    df['week_of_year'] = df.timestamp.dt.weekofyear
+    # 1.1.2013 is Tuesday, so our starting point is the 2nd day of week
+    df['day_of_week'] = df.timestamp.dt.dayofweek + 1
+    df['year'] = df.timestamp.dt.year
+    df["is_wknd"] = df.timestamp.dt.weekday // 4
+    df['is_month_start'] = df.timestamp.dt.is_month_start.astype(int)
+    df['is_month_end'] = df.timestamp.dt.is_month_end.astype(int)
+    return df
 
 def process_data(input_file_path, output_file_path):
     df = pd.read_csv(input_file_path)
     df = df.iloc[:, 1:]
 
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
     df['PM2.5'].fillna(value = df['PM2.5'].mean(), inplace = True)
     df['humidity'].fillna(value=df['humidity'].mean(), inplace=True)
     df['temperature'].fillna(value=df['temperature'].mean(), inplace=True)
@@ -39,15 +56,23 @@ def process_data(input_file_path, output_file_path):
 
     # df[col_names] = ct.fit_transform(df[col_names])
 
+    # df = create_date_features(df)
+
     os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
     df.to_csv(output_file_path, index=False)
 
 def main():
     output_path = INPUT_PATH.replace('raw', 'processed')
-    for file_name in os.listdir(INPUT_PATH):
-        input_file_name = os.path.join(INPUT_PATH, file_name)
-        output_file_path = os.path.join(output_path, file_name)
-        process_data(input_file_name, output_file_path)
+    for dir in os.listdir(INPUT_PATH):
+        for file_name in os.listdir(os.path.join(INPUT_PATH, dir)):
+            input_file_name = os.path.join(INPUT_PATH, dir, file_name)
+            output_file_path = os.path.join(output_path, dir, file_name)
+            process_data(input_file_name, output_file_path)
+
+    # for file_name in tqdm(os.listdir(INPUT_PATH)):
+    #     input_file_name = os.path.join(INPUT_PATH, file_name)
+    #     output_file_path = os.path.join(output_path, file_name)
+    #     process_data(input_file_name, output_file_path)
 
 if __name__ == '__main__':
     main()
---
+++ src/inference.py
@@ -0,0 +1,109 @@
+import numpy as np
+import pandas as pd
+import lightgbm as lgb
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+import os
+from tqdm import tqdm
+import warnings
+
+TEST_DIR = 'data/processed/public-test/input'
+
+def load_input_data(data_dir):
+    df = pd.read_csv(data_dir)
+    data = df['PM2.5'].values.reshape(1, -1)
+    return data
+
+
+def load_models():
+    models = dict()
+    saved_dir = 'saved/models'
+    for model_name in os.listdir(saved_dir):
+        model = xgb.XGBRegressor()
+        model.load_model(os.path.join(saved_dir, model_name))
+        models[model_name[:-5]] = model
+        print(f'Loaded model {model_name[:-5]}')
+    return models
+
+
+def generate_forcast_in_n_hour(model, history, hour=24):
+    # Recursive predict t steps
+    _history = history
+    preds = []
+    for i in range(24):
+
+        prediction = model.predict(_history).tolist()[0]
+        preds.append(prediction)
+
+        _temp = _history.reshape(-1).tolist()
+        _temp = _temp[1:]
+        _temp.append(prediction)
+
+        _history = np.array(_temp).reshape(1, -1)
+
+        # print(_)
+        # print(_history)
+
+    return preds
+
+
+# best neighbor 3
+def forcast_day_based_on_neighbor(X, y, features):
+    neigh = KNeighborsRegressor(n_neighbors=3)
+    neigh.fit(X, y)
+    # print('PRFF', features)
+    return neigh.predict(features)
+
+def to_submission(k_fold, list_df):
+    for idx, df in enumerate(list_df):
+        name = 'res' + '_' + str(k_fold) + '_' + str(idx+1) + '.csv'
+        path = os.path.join('results', k_fold, name)
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        # print(df.tolist())
+        df = pd.DataFrame({'PM2.5':df.tolist()})
+        df.to_csv(path, index=False)
+
+def main():
+
+    models = load_models()
+    # print(models.keys())
+    for k_dir in tqdm(os.listdir(TEST_DIR)):
+        # data per fold
+        data = pd.read_csv('data/raw/data-train/location.csv')
+
+        data = data.assign(T1="", T2="", T3="", T4="", T5="", T6="", T7="", T8="", T9="", T10="", T11="", T12="", T13="", T14="", T15="", T16="", T17="", T18="", T19="", T20="", T21="", T22="", T23="", T24="",)
+
+        for file_name in os.listdir(os.path.join(TEST_DIR, k_dir)):
+            model_name = file_name[:-4]
+            full_file_name = os.path.join(TEST_DIR, k_dir, file_name)
+            input_data = load_input_data(full_file_name)
+            # print(input_data.shape)
+            result = generate_forcast_in_n_hour(models[model_name], input_data)
+
+            data.loc[data['station'] == model_name, -24:] = result
+            
+        # for target_station in data['station']
+
+        # print(data.tail(4))
+        target_stations = data['station'].tolist()[-4:]
+        for idx, target_station in enumerate(target_stations):
+        #   print(target_station, data.loc[data['station'] == target_station])  
+            lon = data.loc[data['station'] == target_station]['longitude'].tolist()[0]
+            lat = data.loc[data['station'] == target_station]['latitude'].tolist()[0]
+            target_feature = [[lon, lat]]
+            for Tday in data.columns[4:]:
+                # Build data
+                X = [[x,y] for x, y in zip(data['longitude'].tolist(), data['latitude'].tolist())][:11]
+                y = data[Tday].tolist()[:11]
+                data.loc[data['station'] == target_station, Tday] = forcast_day_based_on_neighbor(X, y, target_feature)
+        list_df = [data.iloc[i, -24:] for i in range(10, 14)]
+        # print(k_dir)
+        to_submission(k_dir, list_df)
+        # break
+
+
+if __name__ == '__main__':
+    main()

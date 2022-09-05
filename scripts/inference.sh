dc24cc02b167ae4ba7effe89bbaf7144447b3f1c
Hung Nguyen Tien <hungnt@HUNGNT.localdomain>
2022-09-05 23:18:00 +0700

    adjust training code and merge inference files

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,7 +1,8 @@
-python3 -W ignore tools/inference.py --weights_folder get_latest_folder --results_folder data/final-results
+python3 -W ignore tools/inference.py --weights_folder get_latest_folder --results_folder data/final-results --inference_method idw
 # python3 -W ignore tools/inference_full.py --weights_folder saved/lstm_9 --results_folder data/final-results
 cd data/final-results
 rm prediction.zip
 zip -rq prediction.zip *
 cd ..
+mkdir final-output
 cp final-results/prediction.zip final-output/
--- tools/train.py
+++ tools/train.py
@@ -3,6 +3,7 @@ import argparse
 import yaml
 import os
 import glob
+from shutil import copy
 
 from air_forecaster.utils.dataloader import load_data
 from air_forecaster.trainer import Trainer
@@ -10,24 +11,25 @@ from air_forecaster.trainer import Trainer
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config_path", default="config/config.yaml")
-    parser.add_argument("--saved_runs_folder", default="saved/")
+    parser.add_argument("--config_path", default="config/config.yaml", help="config file path")
+    parser.add_argument("--saved_runs_folder", default="saved/", help="parent folder to store all runs log folder")
     return parser.parse_args()
-
 
 def train(args):
     config = yaml.load(open(args.config_path, 'r'), Loader=yaml.Loader)
     pprint.PrettyPrinter(indent=2).pprint(config)
 
     train_id = f"{config['method_name']}_{len(glob.glob(os.path.join(args.saved_runs_folder, '*/')))}"
-    if not os.path.exists(os.path.join(args.saved_runs_folder, train_id)):
-        os.makedirs(os.path.join(args.saved_runs_folder, train_id))
+    saved_folder = os.path.join(args.saved_runs_folder, train_id)
+    if not os.path.exists(saved_folder):
+        os.makedirs(saved_folder)
 
-    # save train config for model loading when inference
+    # save train config and train location coordinates for model loading when inference
+    copy(os.path.join(config['dataset']['train']['args']['path'], "location.csv"),
+         os.path.join(saved_folder, "train-location.csv"))
     save_config = True
     if (save_config):
-        saved_config_path = os.path.join(
-            args.saved_runs_folder, train_id, "train_config.yaml")
+        saved_config_path = os.path.join(saved_folder, "train_config.yaml")
         with open(saved_config_path, 'w') as yaml_file:
             yaml.dump(config, yaml_file, default_flow_style=False)
 
@@ -41,18 +43,15 @@ def train(args):
         # Step 2: create trainers
         for model_name in config['models']:
             model_config = config['models'][model_name]
-            saved_model_path = os.path.join(
-                args.saved_runs_folder, train_id, "models", model_name, station_name + ".json")
+            saved_model_path = os.path.join(saved_folder, "models", model_name, station_name + ".json")
 
             # create each trainer for each type of model
             print("Create", model_name, " trainer for ", station_name)
             trainer = Trainer(model_config)
 
             # start training
-            print("X_train shape", X_train.shape)
-            print("y_train shape", y_train.shape)
-            trainer.train((X_train, y_train),
-                          (X_valid, y_valid), saved_model_path)
+            trainer.train((X_train, y_train), (X_valid, y_valid), saved_model_path)
+        print(f"Training completed, process was saved in {saved_folder}")
 
 
 if __name__ == "__main__":

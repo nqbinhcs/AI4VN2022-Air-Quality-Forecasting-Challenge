9d120e06ea2f8919ae6a5a46cf4db8e244121df1
Binh Q. Nguyen <quangbinh047@gmail.com>
2022-09-07 21:54:59 +0700

    Update preprocess_data for private test

--- scripts/preprocess_data.sh
+++ scripts/preprocess_data.sh
@@ -1,5 +1,7 @@
 python3 -W ignore tools/preprocess_data.py \
---data_train_folder_path="data/train/raw/" \
---preprocessed_data_train_folder_path="data/train/preprocessed/" \
+--type="private-test" \
+--method='interpolate' \
+--data_train_folder_path="data/private-train/raw/" \
+--preprocessed_data_train_folder_path="data/private-train/preprocessed/interpolate-method" \
 --public_test_folder_path="data/public-test/raw/" \
 --preprocessed_public_test_folder_path="data/public-test/preprocessed/"
--- tools/preprocess_data.py
+++ tools/preprocess_data.py
@@ -8,6 +8,10 @@ from shutil import copy
 
 def get_parser():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--type",
+                        default="public-test", help="Available types: public-test, private-test")
+    parser.add_argument("--method",
+                        default="mean", help="Available methods: mean, interpolate")
     parser.add_argument("--data_train_folder_path", default="data/train/raw")
     parser.add_argument("--preprocessed_data_train_folder_path",
                         default="data/train/preprocessed")
@@ -15,7 +19,15 @@ def get_parser():
                         default="data/public-test/raw")
     parser.add_argument("--preprocessed_public_test_folder_path",
                         default="data/public-test/preprocessed")
+
     return parser.parse_args()
+
+
+def convert_to_true_format(input_file_path, type):
+    if type == 'private-test':
+        df = pd.read_csv(input_file_path)
+        df.rename(columns={'time': 'timestamp'}, inplace=True)
+        df.to_csv(input_file_path, index=False)
 
 
 def create_date_features(df):
@@ -32,18 +44,7 @@ def create_date_features(df):
     return df
 
 
-def process_data_interpolate(input_file_path, output_file_path):
-    df = pd.read_csv(input_file_path)
-    df = df.iloc[:, 1:]
-    df['timestamp'] = pd.to_datetime(df['timestamp'])
-    df['PM2.5'].interpolate(method='linear', inplace=True)
-    df['humidity'].interpolate(method='linear', inplace=True)
-    df['temperature'].interpolate(method='linear', inplace=True)
-    os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
-    df.to_csv(output_file_path, index=False)
-
-
-def process_data(input_file_path, output_file_path):
+def process_data_mean(input_file_path, output_file_path):
     df = pd.read_csv(input_file_path)
     df = df.iloc[:, 1:]
     df['timestamp'] = pd.to_datetime(df['timestamp'])
@@ -66,21 +67,33 @@ def process_data_interpolate(input_file_path, output_file_path):
 
 
 def preprocess_data(args):
+    type = args.type
+    method = args.method
     data_train_folder_path = args.data_train_folder_path
     preprocessed_data_train_folder_path = args.preprocessed_data_train_folder_path
     public_test_folder_path = args.public_test_folder_path
     preprocessed_public_test_folder_path = args.preprocessed_public_test_folder_path
 
+    print(f'Type data: {type}')
+    print(f'Method: {method}')
+
     # preprocessing data train
-    need_to_preprocess_sub_dirs = ["input", "output"]
+    need_to_preprocess_sub_dirs = [
+        "input", "output"] if type == 'public-test' else ["input"]
     for sub_dir in need_to_preprocess_sub_dirs:
         print(f"Preprocessing files in {sub_dir} in data train...")
         for input_file_path in tqdm(glob.glob(os.path.join(data_train_folder_path, f"{sub_dir}/*"))):
             file_name = os.path.basename(input_file_path)
             output_file_path = os.path.join(
                 preprocessed_data_train_folder_path, f"{sub_dir}", file_name)
-            # process_data_interpolate(input_file_path, output_file_path)
-            process_data(input_file_path, output_file_path)
+
+            # convert to true format if private test
+            convert_to_true_format(input_file_path, type)
+
+            if method == 'interpolate':
+                process_data_interpolate(input_file_path, output_file_path)
+            elif method == 'mean':
+                process_data_mean(input_file_path, output_file_path)
 
     # copy `location.csv` file to the new preprocessed data train folder
     copy(os.path.join(data_train_folder_path, "location.csv"),
@@ -88,18 +101,19 @@ def preprocess_data(args):
 
     # preprocessing public test
     # `input` files
-    print(f"Preprocessing files in public test...")
-    for sub_dir in tqdm(glob.glob(os.path.join(public_test_folder_path, "input/*/"))):
-        for input_file_path in glob.glob(sub_dir + "*"):
-            file_name = os.path.basename(input_file_path)
-            sub_dir_name = os.path.basename(os.path.normpath(sub_dir))
-            output_file_path = os.path.join(
-                preprocessed_public_test_folder_path, "input", sub_dir_name, file_name)
-            process_data(input_file_path, output_file_path)
-            # process_data_interpolate(input_file_path, output_file_path)
-    # copy `location.csv` file to the new preprocessed public test folder
-    copy(os.path.join(public_test_folder_path, "location.csv"),
-         preprocessed_public_test_folder_path)
+    if type == 'public-test':
+        print(f"Preprocessing files in data test...")
+        for sub_dir in tqdm(glob.glob(os.path.join(public_test_folder_path, "input/*/"))):
+            for input_file_path in glob.glob(sub_dir + "*"):
+                file_name = os.path.basename(input_file_path)
+                sub_dir_name = os.path.basename(os.path.normpath(sub_dir))
+                output_file_path = os.path.join(
+                    preprocessed_public_test_folder_path, "input", sub_dir_name, file_name)
+                process_data_mean(input_file_path, output_file_path)
+                # process_data_interpolate(input_file_path, output_file_path)
+        # copy `location.csv` file to the new preprocessed public test folder
+        copy(os.path.join(public_test_folder_path, "location.csv"),
+             preprocessed_public_test_folder_path)
 
 
 if __name__ == "__main__":

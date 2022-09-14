df22019ead5ef4b884f1b1e9b8bef689b90b3bc9
Nguyen Tien Hung <nguyentienhung.quynhon@gmail.com>
2022-09-14 19:36:34 +0700

    refactoring final code

--- tools/inference_private.py
+++ tools/inference_private.py
@@ -16,24 +16,35 @@ from air_forecaster.utils.getter import get_instance
 
 def get_parser():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--phase", default="public-test")
     parser.add_argument("--weights_folder", default="get_latest_folder")
     parser.add_argument("--results_folder", default="data/final-results")
     parser.add_argument("--drop_null_values", default=False)
-    parser.add_argument("--inference_method", default="kriging",
+    parser.add_argument("--inference_method", default="kriging", 
                         help="Available methods: knn, kriging, idw")
     return parser.parse_args()
-
 
 def load_config(config_path):
     config = yaml.load(open(config_path, 'r'), Loader=yaml.Loader)
     return config
 
-
-def load_input_data(data_dir):
-    df = pd.read_csv(data_dir)
-    data = df['PM2.5'].values.reshape(1, -1)
-    return data
-
+def load_input_data(data_dir, use_lstm):
+    if use_lstm:
+        df = pd.read_csv(data_dir)
+        pm = df['PM2.5'].values
+        humidity = df['humidity'].values
+        temperature = df['temperature'].values
+        # reshape
+        pm = pm.reshape((pm.shape[0], 1))
+        humidity = humidity.reshape((humidity.shape[0], 1))
+        temperature = temperature.reshape((temperature.shape[0], 1))
+        dataset = np.hstack((humidity, temperature, pm))
+        history = dataset.reshape((-1, 168, 3))
+        return history
+    else:
+        df = pd.read_csv(data_dir)
+        data = df['PM2.5'].values.reshape(1, -1)
+        return data
 
 def load_models(weights_folder):
     all_models = {}
@@ -53,7 +64,6 @@ def load_models(weights_folder):
         }
     return all_models
 
-
 def check_all_null(data_dir):
     df = pd.read_csv(data_dir)
     null_pm25 = df['PM2.5'].isnull().sum()
@@ -63,33 +73,31 @@ def check_all_null(data_dir):
             null_temperature == len(df['temperature']) or
             null_humidity == len(df['humidity']))
 
-
-def generate_forecast_in_n_hour(model, history, hour=24):
+def generate_forecast_in_n_hour(model, history, use_lstm, hour=24):
     # Recursively predict t steps
-    _history = history
-    predictions = []
-    for i in range(24):
+    if use_lstm:
+        print("Prediction")
+        y_hat = [x[2] for x in model.predict(history).tolist()[0]]
+        return y_hat
+    else:
+        _history = history
+        predictions = []
+        for i in range(24):
+            prediction = model.predict(_history).tolist()[0]
+            predictions.append(prediction)
+            _temp = _history.reshape(-1).tolist()
+            _temp = _temp[1:]
+            _temp.append(prediction)
+            _history = np.array(_temp).reshape(1, -1)
+        return predictions
 
-        prediction = model.predict(_history).tolist()[0]
-        predictions.append(prediction)
-
-        _temp = _history.reshape(-1).tolist()
-        _temp = _temp[1:]
-        _temp.append(prediction)
-
-        _history = np.array(_temp).reshape(1, -1)
-
-    return predictions
-
-
-def forecast_day_based_on_knn(X, y, target_features, n_neighbor):
-    neigh = KNeighborsRegressor(
-        n_neighbors=n_neighbor, weights='distance', n_jobs=-1)
+def forecast_day_based_on_knn(X, y, target_station_coordinates, n_neighbors):
+    neigh = KNeighborsRegressor(n_neighbors=n_neighbors, weights='distance', n_jobs=-1)
     neigh.fit(X, y)
-    return neigh.predict(target_features)
+    return neigh.predict(target_station_coordinates)
 
 
-def forecast_day_based_on_kriging(X, y, features):
+def forecast_day_based_on_kriging(X, y, target_station_coordinates):
     data = np.array([[X[idx][0], X[idx][1], y[idx]] for idx in range(len(X))])
     OK = UniversalKriging(
         data[:, 0],
@@ -100,7 +108,7 @@ def forecast_day_based_on_kriging(X, y, features):
         enable_plotting=False,
         nlags=1,
     )
-    z, ss = OK.execute("points", features[0][0], features[0][1])
+    z, ss = OK.execute("points", target_station_coordinates[0][0], target_station_coordinates[0][1])
     return [z[0] + 7.5]
 
 
@@ -116,11 +124,12 @@ def forecast_day_based_on_idw(X, y, target_features):
 
 
 def export_submission(k_fold, list_df, results_folder):
+    if not os.path.exists(results_folder):
+        os.makedirs(results_folder)
     for index, df in enumerate(list_df):
         name = 'res' + '_' + str(k_fold) + '_' + str(index + 1) + '.csv'
         path = os.path.join(results_folder, k_fold, name)
         os.makedirs(os.path.dirname(path), exist_ok=True)
-        # print(df.tolist())
         df = pd.DataFrame({'PM2.5': df.tolist()})
         df.to_csv(path, index=False)
 
@@ -132,80 +141,74 @@ def inference(args):
     config = load_config(os.path.join(
         args.weights_folder, "train_config.yaml"))
 
+    print("Loading models")
     all_models = load_models(args.weights_folder)
-
-    test_dir = os.path.join(
-        config['dataset']['test']['args']['path'], "input/")
+    print("Models loaded")
+    test_dir = os.path.join(config['dataset']['test']['args']['path'], "input/")
 
     print('Testing...')
     for k_dir in tqdm(os.listdir(test_dir)):
-        # load data per fold
-
-        location_input = os.path.join(test_dir, k_dir, 'location_input.csv')
-        location_output = os.path.join(test_dir, k_dir, 'location_output.csv')
-
-        data_location_input = pd.read_csv(location_input)
-        data_location_output = pd.read_csv(location_output)
-
         # load train location coordinates
-        # data = pd.read_csv(os.path.join(
-        #     args.weights_folder, "train-location.csv"))
-        data = pd.concat([data_location_input, data_location_output])
+        if (args.phase == "public-test"):
+            data = pd.read_csv(os.path.join(args.weights_folder, "train-location.csv"))
+        else:
+            location_input = os.path.join(test_dir, k_dir, 'location_input.csv')
+            location_output = os.path.join(test_dir, k_dir, 'location_output.csv')
+            data_location_input = pd.read_csv(location_input)
+            data_location_output = pd.read_csv(location_output)
+            num_target_stations = data_location_output.shape[0]
+            data = pd.concat([data_location_input, data_location_output])
 
+
+        data.reset_index(inplace=True)
         data = data.assign(T1="", T2="", T3="", T4="", T5="", T6="", T7="", T8="", T9="", T10="", T11="", T12="",
                            T13="", T14="", T15="", T16="", T17="", T18="", T19="", T20="", T21="", T22="", T23="", T24="",)
 
         list_df = []
         for model_name in all_models:
+            use_lstm = (model_name == "LSTM")
             model_result_ratio = all_models[model_name]["result_ratio"]
             station_models = all_models[model_name]["station_models"]
 
-            for file_name in os.listdir(os.path.join(test_dir, k_dir)):
-
-                # skip location files
-                if file_name in ['location_input.csv', 'location_output.csv']:
+            for filepath in glob.glob(os.path.join(test_dir, k_dir, "*.csv")):
+                filename = os.path.basename(filepath)
+                if filename in ['location_input.csv', 'location_output.csv']:
                     continue
 
-                station_name = file_name.replace('.csv', '')
-                full_file_name = os.path.join(test_dir, k_dir, file_name)
+                station_name = filename.replace('.csv', '')
 
-                if (args.drop_null_values == True and check_all_null(full_file_name)) or (station_name not in station_models):
+                if (args.drop_null_values == True and check_all_null(filepath)) or (station_name not in station_models):
                     data = data.drop(
                         data.index[data['location'] == station_name])
                 else:
-                    input_data = load_input_data(full_file_name)
-                    result = generate_forecast_in_n_hour(
-                        station_models[station_name], input_data)
+                    input_data = load_input_data(filepath, use_lstm)
+                    result = generate_forecast_in_n_hour(station_models[station_name], input_data, use_lstm)
                     data.loc[data['location'] == station_name, -24:] = result
 
-            target_stations = data['location'].tolist()[-6:]
+            target_stations = data['location'].tolist()[-num_target_stations:]
             for idx, target_station in enumerate(target_stations):
-                lon = data.loc[data['location'] == target_station]['longitude'].tolist()[
-                    0]
-                lat = data.loc[data['location'] == target_station]['latitude'].tolist()[
-                    0]
-                target_feature = [[float(lon), float(lat)]]
-                # print(target_feature)
+                lon = data.loc[data['location'] == target_station]['longitude'].tolist()[0]
+                lat = data.loc[data['location'] == target_station]['latitude'].tolist()[0]
+                target_station_coordinates = [[float(lon), float(lat)]]
+
                 for today in data.columns[3:]:
                     # Build data
                     X = [[float(x), float(y)] for x, y in zip(
-                        data['longitude'].tolist(), data['latitude'].tolist())][:-6]
-                    y = data[today].tolist()[:-6]
-                    # print(y)
+                        data['longitude'].tolist(), data['latitude'].tolist())][:-num_target_stations]
+                    y = data[today].tolist()[:-num_target_stations]
+
                     if args.inference_method == "knn":
-                        n_neighbor = config['dataset']['test']['args']['n_neighbor']
-                        data.loc[data['location'] == target_station, today] = forecast_day_based_on_knn(
-                            X, y, target_feature, n_neighbor)
+                        n_neighbors = config['dataset']['test']['args']['n_neighbor']
+                        prediction = forecast_day_based_on_knn(X, y, target_station_coordinates, n_neighbors)
                     elif args.inference_method == "kriging":
-                        data.loc[data['location'] == target_station, today] = forecast_day_based_on_kriging(
-                            X, y, target_feature)
+                        prediction = forecast_day_based_on_kriging(X, y, target_station_coordinates)
                     elif args.inference_method == "idw":
-                        data.loc[data['location'] == target_station, today] = forecast_day_based_on_idw(
-                            X, y, target_feature)
+                        prediction = forecast_day_based_on_idw(X, y, target_station_coordinates)
                     else:
                         raise NotImplementedError
+                    data.loc[data['location'] == target_station, today] = prediction
 
-            new_list_df = [data.iloc[i, -24:] for i in range(-6, 0)]
+            new_list_df = [data.iloc[i, -24:] for i in range(-num_target_stations, 0)]
             if (list_df == []):
                 list_df = new_list_df
             else:
@@ -217,3 +220,5 @@ def inference(args):
 if __name__ == '__main__':
     args = get_parser()
     inference(args)
+
+
